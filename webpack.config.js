bb4d6ce170af17d3181afbcac9a08dd187613d53
kkgarcia <rus135.ra@gmail.com>
2023-03-22 18:00:57 +0300

    changed subtree for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -23,5 +24,8 @@ module.exports = {
                 type: 'asset/resource'
             },
         ],
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/template/index.html'})
+    ]
 };
